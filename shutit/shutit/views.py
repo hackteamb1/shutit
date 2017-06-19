7808860ddcdacadf9e674118c65cbe47c80bad7c
Lir <locker.lir@gmail.com>
2017-06-19 19:43:01 +0300

    added signup and signout

--- shutit/shutit/views.py
+++ shutit/shutit/views.py
@@ -1,51 +1,44 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework import viewsets, status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .serializers import QueueSerializer, StateSerializer
+from django.contrib.auth import login, logout, authenticate
+from rest_framework import viewsets
+from .serializers import QueueSerializer
 from .models import _Queue as Queue
-from .models import _Passenger as Passenger
 
 def index_view(request):
-    return render(request, 'shutit/index.html')
+    if request.user.is_authenticated():
+        return render(request, 'shutit/index.html')
+    return redirect("login")
 
 def login_view(request):
     if request.POST:
         id_number = request.POST['id_number']
         password = request.POST['password']
-        passenger = authenticate(request, id_number=id_number, password=password)
-        if passenger:
-            login(request, passenger)
-            redirect("index")
+        user = authenticate(request, id_number=id_number, password=password)
+        if user:
+            login(request, user)
+            return redirect("index")
         else:
             return render(request, 'shutit/login.html', context={'wrong': True})
 
-    if request.user.is_authenticated:
-        redirect("index")
+    if request.user.is_authenticated():
+        return redirect("index")
 
     return render(request, 'shutit/login.html')
 
 def signup_view(request):
-    pass
+    return render("shutit/signup.html")
 
-
-class QueueViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = Queue.objects.all()
-    serializer_class = QueueSerializer
-
-
+def signout_view(request):
+    logout(request)
+    return redirect("index")
 
 @api_view(['GET'])
 def passenger_state(request, passenger_id):
     try:
-        passenger_state = Passenger.objects.get(id_number=passenger_id)
+        passenger_state = Queue.objects.filter(
+                        passenger=passenger_id, is_waiting=True).first()
     except ObjectDoesNotExist:
-        content = {'message': 'You are not a user?'}
+        content = {'message': 'You are not in the queue'}
         return Response(content, status=status.HTTP_404_NOT_FOUND)
     serializer = StateSerializer(passenger_state, many=False, context={'request': request})
     return Response(serializer.data)
