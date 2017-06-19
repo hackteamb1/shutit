97ed367f91b6f8eecc7d07f0f580212ef4e78464
Shai <shaikustin@live.com>
2017-06-19 13:10:43 +0300

    added queue model

---
+++ shutit/shutit/models/Queue.py
@@ -0,0 +1,21 @@
+from django.db import models
+from models import Passenger
+
+
+class Queue(models.Model):
+    """Holds the queue of awaiting users."""
+
+    arrival_time = models.DateTimeField()
+    boarding_time = models.DateTimeField(blank=True)
+    is_waiting = models.BooleanField(default=True)
+    passanger = models.ForeignKey(Passenger)
+
+    class Meta:
+        """Meta definition for Queue."""
+
+        verbose_name = 'Queue'
+        verbose_name_plural = 'Queues'
+
+    def __unicode__(self):
+        """Unicode representation of Queue."""
+        pass
